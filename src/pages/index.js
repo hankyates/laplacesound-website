226b419e9f4de4b200fa320a2f9b2565165c9547
Hank Yates <hankyates@gmail.com>
2021-02-24 21:32:58 -0800

    page shuffling

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,6 +42,7 @@ export default function HomeIndex ({
               </header>
               <ul className="home icons">
                 <li><a className="icon fa-headphones" href="/mixing"><span className="label">Mixing</span></a></li>
+                <li><a className="icon fa-music" href="/mastering"><span className="label">Mastering</span></a></li>
                 <li><a className="icon fa-music" href="/composition"><span className="label">Composition</span></a></li>
                 <li><a className="icon fa-music" href="/clients"><span className="label">Clients</span></a></li>
                 <li><a className="icon fa-music" href="/contact"><span className="label">Contact</span></a></li>
